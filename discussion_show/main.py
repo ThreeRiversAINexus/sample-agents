52834d70c50c6068e0529cc09547c77525206cd1
ThreeRiversAINexus <threeriversainexus@gmail.com>
2024-11-09 22:14:15 +0000

    discussion_show - adding OPENAI_API_KEY

--- discussion_show/main.py
+++ discussion_show/main.py
@@ -26,8 +26,9 @@ class AudioRecorder(Element, component="audio_recorder.vue"):
     async def play_recorded_audio(self) -> None:
         self.run_method("playRecordedAudio")
 
-OPENAI_API_KEY=os.getenv("RUNPOD_API_KEY")
-OPENAI_MODEL_NAME=os.getenv("MODEL_NAME")
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+RUNPOD_API_KEY=os.getenv("RUNPOD_API_KEY")
+RUNPOD_MODEL_NAME=os.getenv("MODEL_NAME")
 RUNPOD_ENDPOINT_ID=os.getenv("RUNPOD_ENDPOINT_ID")
 RUNPOD_SDXL_ENDPOINT_ID=os.getenv("RUNPOD_SDXL_ENDPOINT_ID")
 
@@ -84,11 +85,11 @@ class ImageGenerator:
             print("Failed to start the job.")
         return "Failed"
 
-async def on_audio_ready(audio):
+async def on_audio_ready(base64_audio):
     transcriber = AudioTranscriber()
-    print(f"Transcribing audio: {audio}")
+    print(f"Transcribing audio")
 
-    transcription = await transcriber.transcribe(audio)
+    transcription = await transcriber.transcribe(base64_audio)
     print(f"Transcription: {transcription}")
     my_context_buffer = MyContextBuffer()
     my_context_buffer.add_to_context(transcription)
@@ -104,7 +105,7 @@ def update_image(interactive_image, context):
         interactive_image.set_source(image)
         interactive_image.force_reload()
 
-def complete_cycle(interactive_image):
+def complete_cycle(audio_transcriber, my_context_buffer, interactive_image):
     print("Beginning cycle")
     # When audio is ready, transcribe it
     # Add the transcription to the context
@@ -115,143 +116,9 @@ def complete_cycle(interactive_image):
 @ui.page("/")
 async def main():
     ui.label("Hello World")
+    AudioRecorder(on_audio_ready=on_audio_ready)
+    my_context_buffer = MyContextBuffer()
     the_show = ui.interactive_image()
-    ui.timer(interval=60_000, callback=lambda: complete_cycle(the_show))
-
-        # the_show = ui.interactive_image(image).classes('w-full h-full')
-    #ui.timer(interval=5000, callback=lambda: the_show.set_source(image))
-    
-
-    # listener = AudioRecorder(on_audio_ready=on_audio_ready)
+    ui.timer(interval=60_000, callback=lambda: complete_cycle( my_context_buffer, the_show))
 
 ui.run(port=8080)
-
-# This class listens to the microphone
-# and makes SoundBite objects
-# which are then processed and added
-# to the context
-# import asyncio
-# class AudioListener:
-#     def __init__(self, start_recording, stop_recording, seconds_to_listen=15):
-#         self.start_recording = start_recording 
-#         self.stop_recording = stop_recording
-#         self.seconds_to_listen = seconds_to_listen
-#         pass
-# 
-#     async def record(self):
-#         sound_bite = SoundBite()
-#         print(f"Path to sound bite: {sound_bite.file.name}")
-#         await self.start_recording(sound_bite.file.name)
-#         await asyncio.sleep(self.seconds_to_listen)
-#         await self.stop_recording()
-#         return sound_bite
-# 
-#     async def get_contexts(self):
-#         context = MyContext()
-#         while context.is_full_enough() == False:
-#             sound_bite = await self.record()
-#             context.text += sound_bite.to_text()
-#         yield context
-# 
-#     
-# # Inspiration from https://gist.github.com/arctic-hen7/5580d5452f77d4e6b206caf90f8d73c6
-# 
-# # This class represents a sound clip
-# # in a temp file
-# class SoundBite:
-#     def __init__(self):
-#         self.file = tempfile.NamedTemporaryFile(suffix=".wav")
-# 
-#     def to_text(self):
-#         # Use the whisper API
-#         return ""
-#     
-# 
-# # This represents a context that
-# # the chat agent can use to generate
-# # the image
-# class MyContext:
-#     def __init__(self):
-#         self.text = ""
-#         self.full_enough = 250
-#         pass
-# 
-#     def is_full_enough(self):
-#         if len(self.text) > self.full_enough:
-#             return True
-#         else:
-#             return False
-# 
-# # This class generates images from
-# # a context
-# class ImageGenerator:
-#     def __init__(self):
-#         pass
-# 
-#     def generate_image_from_context(self, context):
-#         generated_image = GeneratedImage()
-#         return generated_image
-#         # Use langchain to turn the context
-#         # into a suitable prompt for an image
-#         # generator
-# 
-#         # Then use the image generator API via runpod
-#         # to generate an image, and put the data
-#         # into a GeneratedImage object
-# 
-# # This represents a generated image
-# # that can be displayed in
-# # base64 format
-# class GeneratedImage:
-#     def __init__(self):
-#         self.base64 = ""
-#         self.file_type = "png"
-#         pass
-#     
-# 
-# async def main(page: ft.Page):
-#     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-#     display = ft.ListView(auto_scroll=True, expand=True)
-#     # Add a button to enable or disable audio listening
-#     # as a toggle with an icon for mute/unmute
-# 
-#     # Show a meter that displays the amount of context
-#     # that has been generated so far
-# 
-#     # The objective is to continuously listen to the microphone
-#     # and use the openai whisper API to convert speech to text
-#     # and then use the text to generate an image
-# 
-#     audio_rec = ft.AudioRecorder(
-#         audio_encoder=ft.AudioEncoder.WAV,
-#     )
-#     page.add(audio_rec)
-# 
-#     async def start_recording(file_path):
-#         audio_rec.start_recording(file_path)
-# 
-#     async def stop_recording():
-#         audio_rec.stop_recording()
-# 
-#     seconds_to_listen = 5
-#     listener = AudioListener(start_recording=start_recording, stop_recording=stop_recording, seconds_to_listen=seconds_to_listen)
-#     image_generator = ImageGenerator()
-# 
-#     async for context in listener.get_contexts():
-#         print(f"Context: {context.text}")
-#         generated_image = image_generator.generate_image_from_context(context)
-#         # display image here
-# 
-#     # while True:
-#     #     while context.is_full_enough() == False:
-#     #         sound_bite = listener.record()
-#     #         context.text += sound_bite.to_text()
-#     #     generated_image = image_generator.generate_image_from_context(context)
-# 
-#     page.add(
-#         display
-#     )
-# 
-#     page.update()
-# 
-# ft.app(target=main)
